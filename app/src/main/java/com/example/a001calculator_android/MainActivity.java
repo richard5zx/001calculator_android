1fb225734dc612914adff8f95d928138484bed81
richard5zx <richardchao2009@gmail.com>
2024-10-30 18:58:47 +0800

    cal app update

--- app/src/main/java/com/example/a001calculator_android/MainActivity.java
+++ app/src/main/java/com/example/a001calculator_android/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.a001calculator_android;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Attributes
+     private EditText etA, etB;
+     private TextView tvOper, tvResult;
+     private Button[] btns = new Button[6];
+     private int[] btnIds = {R.id.btnAdd, R.id.btnSub, R.id.btnMul, R.id.btnDiv, R.id.btnCal, R.id.btnReset};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,21 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        initView();
+    }
+
+    private void initView() {
+        etA = findViewById(R.id.etA);
+        etB = findViewById(R.id.etB);
+        tvOper = findViewById(R.id.tvOper);
+        tvResult = findViewById(R.id.tvResult);
+        for (int i = 0; i < btns.length; i++) {
+            btns[i] = findViewById(btnIds[i]);
+        }
+    }
+
+    public void onClick(View view) {
+
     }
 }
